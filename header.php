029dace126050fc98605cf64e9e052e6d97796c6
fchristiansen <felipe@seo2.cl>
2018-01-15 18:16:53 -0300

    ajustes

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="<?php bloginfo('description'); ?>">
-     <link rel="icon" type="image/png" href="<?php bloginfo('template_url'); ?>/assets/img/favicon.png">
+     <link rel="icon" type="image/png" href="<?php bloginfo('template_url'); ?>/assets/img/favicon.ico">
     <?php if(is_home()) : ?>
     <title><?php bloginfo('name'); ?></title>
     <?php else : ?>
